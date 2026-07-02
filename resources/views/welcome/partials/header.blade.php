632bd3b31fcf48a203ef21180830b8b995e65a92
vdeepak001 <dv63829@gmail.com>
2026-07-02 15:03:09 +0530

    update code

--- resources/views/welcome/partials/header.blade.php
+++ resources/views/welcome/partials/header.blade.php
@@ -85,7 +85,7 @@
                                             <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
                                         </svg>
                                     </button>
-                                    <div class="w-40 rounded-full bg-impetus-teal py-1 text-center text-[14px] tracking-wider text-white shadow-md font-outfit">
+                                    <div class="w-40 rounded-full bg-impetus-teal py-1 text-center tracking-wider text-white shadow-md font-outfit" style="font-size: 14px;">
                                         UID: {{ auth()->user()->unique_sequence_number ?? 'N/A' }}
                                     </div>
                                     <div x-show="userMenuOpen" x-cloak x-transition
